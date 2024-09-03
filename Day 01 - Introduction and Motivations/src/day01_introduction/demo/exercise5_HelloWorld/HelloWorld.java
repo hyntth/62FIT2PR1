e9d98ac6d81d7456aa9e27bad8a0012776a5ad72
hyntth <thuhuyen20092004@gmail.com>
2024-09-03 23:28:56 +0700

    Upload Tut01

--- Day 01 - Introduction and Motivations/src/day01_introduction/demo/exercise5_HelloWorld/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package day01_introduction.demo.exercise5_HelloWorld;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		// Print out "Hello, World"
-		System.out.println("Hello, World!"); //
-	}
-}
---
+++ Day 01 - Introduction and Motivations/src/day01_introduction/exercise6_ComputeExpressions/ComputeExpressions.java
@@ -0,0 +1,9 @@
+package day01_introduction.exercise6_ComputeExpressions;
+
+public class ComputeExpressions {
+    public static void main(String[] args) {
+        System.out.println((12.5 * 5.0 + 5.5) / (4.5 + 2.0 * 3.5));
+        System.out.println((12.5 * 5.0 + 5.5) / (4.5 + 2.0 * 3.5));
+    }
+}
+
